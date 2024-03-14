5318c5f7a818bf5a3138bd40abb7d6821a15890e
Irmansyah <88528922+irmansyah38@users.noreply.github.com>
2024-03-14 17:10:57 +0700

    percobaan

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,10 @@
 #include <Arduino.h>
-#include <LiquidCrystal_I2C.h>
+#include "ScaleClass.h"
 
 // make object lcd
-LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 void setup()
 {
-
-  lcd.init();
-  lcd.backlight();
-  displaySensors();
 }
 
 void loop()
