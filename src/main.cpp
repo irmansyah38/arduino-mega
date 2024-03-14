4a1f0bdf9e3722b5006f4bef3d8c10d4e428e6de
Irmansyah <88528922+irmansyah38@users.noreply.github.com>
2024-03-14 17:20:51 +0700

    coba

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,15 @@
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
-#include "PumpClass.h"
-#include "ScaleClass.h"
 #include "TemperatureClass.h"
 
 // make object sensors
 DS18B20Sensor suhu(2);
-ScaleClass basaScale(3, 4, 8000);
-ScaleClass asamScale(6, 7, 9000);
-PumpClass pump(10);
+
 // make object lcd
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 // make variable for value sensors
-int phValue = 0;
 int suhuValue = 0;
-float basaValue = 0;
-float asamValue = 0;
 
 void displaySensorsValue(int coloum, int row, float value)
 {
@@ -27,25 +20,11 @@ void displaySensorsValue(int coloum, int row, float value)
 // make function for postion display sensors
 void displaySensors()
 {
-  // display ph
-  lcd.setCursor(6, 0);
-  lcd.print("PH   : ");
-  displaySensorsValue(7, 0, phValue);
 
   // display suhu
   lcd.setCursor(6, 1);
   lcd.print("Suhu : ");
   displaySensorsValue(7, 1, suhuValue);
-
-  // display basa scale
-  lcd.setCursor(6, 2);
-  lcd.print("Basa : ");
-  displaySensorsValue(7, 2, basaValue);
-
-  // display basa scale
-  lcd.setCursor(6, 2);
-  lcd.print("Asam : ");
-  displaySensorsValue(7, 3, asamValue);
 }
 
 // display sensors value for example suhu,ph dan lain lain
@@ -64,37 +43,11 @@ void executeSuhu()
   if (suhu.checkSensor())
   {
     suhuValue = suhu.getValue();
-    displaySensorsValue(7, 1, suhuValue)
+    displaySensorsValue(7, 1, suhuValue);
   }
   else
   {
     displaySensorError("suhu");
-  }
-}
-
-void executeBasa()
-{
-  if (basaScale.checkSensor())
-  {
-    basaValue = basaScale.getValue();
-    displaySensorsValue(7, 2, basaValue)
-  }
-  else
-  {
-    displaySensorError("Basa");
-  }
-}
-
-void executeAsam()
-{
-  if (asamScale.checkSensor())
-  {
-    asamValue = asamScale.getValue();
-    displaySensorsValue(7, 2, asamValue)
-  }
-  else
-  {
-    displaySensorError("asamScale");
   }
 }
 
@@ -104,10 +57,8 @@ void setup()
   // for initialize
   suhu.initialize();
   // for initialize scale
-  basaScale.initialize();
-  asamScale.initialize();
+
   // for pump
-  pump.initialize();
   // lcd
   lcd.init();
   lcd.backlight();
@@ -117,6 +68,4 @@ void setup()
 void loop()
 {
   executeSuhu();
-  executeBasa();
-  executeAsam();
 }
